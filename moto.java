335a7985c9ba38187b07bc9dac18ea10bed50a75
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 15:20:46 -0500

    Se muestran los datos base de la clase hija auto

--- moto.java
+++ moto.java
@@ -4,4 +4,8 @@ public class moto extends vehiculo {
     public moto(String marca, int velocidadMax, boolean tieneMaletero) {
         super(marca, velocidadMax);
         this.tieneMaletero = tieneMaletero;
+    }public void mostrarMoto() {
+        mostrarDatos();
+        System.out.println("Tiene maletero: " + (tieneMaletero? "Sí" : "No"));
+    }
     }
