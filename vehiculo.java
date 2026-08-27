61989118219a76f47ef58c0d8da12c13fe7154fb
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 15:08:24 -0500

    A la clase vehiculo se realizo el metodo constructor

---
+++ vehiculo.java
@@ -0,0 +1,10 @@
+public class vehiculo {
+    String marca;
+    int velocidadMax;
+
+    
+    public vehiculo(String marca, int velocidadMax) {
+        this.marca = marca;
+        this.velocidadMax = velocidadMax;
+    }
+}
